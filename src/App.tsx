6b886a7d47761e278aa3534064814fc9cd544dd6
LGalushka <liliya2615@gmail.com>
2026-01-20 20:55:22 +0300

    feat: создана структура проекта и типизированный компонент UserCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import UserCard from "./components/UserCard";
 
 const App: React.FC = () => {
   return (
-    <div className="container">
-      <h1>Привет, это мой первый React проект!</h1>
-      <p>Настройка завершена успешно</p>
+    <div style={{ padding: '40px', fontFamily: 'Arial'}}>
+      <h1>Команда проекта</h1>
+      <div style={{ display: 'flex' }}>
+        {/**Передаем данные серез Props.
+         * Попробуй специально передать число в name TS подсветит ошибку!
+         */}
+        <UserCard
+        name="Алексей"
+        role="Frontend Developer"
+        isOnline={true}
+        />
+        <UserCard
+        name="Лилия"
+        role="UI/UX Designer"
+        isOnline={false}
+        />
+      </div>
     </div>
   );
 };
